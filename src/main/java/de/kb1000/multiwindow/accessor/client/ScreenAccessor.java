d90749fa56b5e47c0a393c83626335dbf6135704
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-02-25 11:34:50 +0100

    Update BreakoutAPI to 1.0.1

--- src/main/java/de/kb1000/multiwindow/accessor/client/ScreenAccessor.java
+++ src/main/java/de/kb1000/multiwindow/accessor/client/ScreenAccessor.java
@@ -1,9 +1,20 @@
-package de.kb1000.multiwindow.accessor.client;
+package de.kb1000.multiwindow.mixin.client;
 
 import de.kb1000.multiwindow.client.gui.ScreenBreakout;
+import net.fabricmc.api.EnvType;
+import net.fabricmc.api.Environment;
+import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.gui.screen.option.OptionsScreen;
 import net.minecraft.util.Identifier;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
+@Mixin(OptionsScreen.class)
+@Environment(EnvType.CLIENT)
 public interface ScreenAccessor {
     Identifier multi_window_getBreakoutId();
     ScreenBreakout multi_window_getBreakout();
+
+    @Accessor
+    Screen getParent();
 }
