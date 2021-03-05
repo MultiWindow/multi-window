18650c6527eaf6135a812e1e212b8d75ed838fd6
kb1000 <kaeptmblaubaer1000@gmail.com>
2021-03-05 15:28:52 +0100

    Forward mouse button events to the Screen

--- src/main/java/de/kb1000/multiwindow/client/gui/ScreenBreakout.java
+++ src/main/java/de/kb1000/multiwindow/client/gui/ScreenBreakout.java
@@ -1,15 +1,19 @@
 package de.kb1000.multiwindow.client.gui;
 
+import com.mojang.blaze3d.systems.RenderSystem;
 import com.raphydaphy.breakoutapi.breakout.Breakout;
 import com.raphydaphy.breakoutapi.breakout.window.BreakoutWindow;
+import de.kb1000.multiwindow.accessor.client.ScreenAccessor;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.fabricmc.fabric.api.client.screen.v1.Screens;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.util.GlfwUtil;
 import net.minecraft.client.util.InputUtil;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
+import org.jetbrains.annotations.NotNull;
 import org.lwjgl.glfw.GLFWDropCallback;
 
 import java.nio.file.Path;
@@ -19,23 +23,30 @@ import java.util.List;
 import java.util.Queue;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import static org.lwjgl.glfw.GLFW.GLFW_MOD_CONTROL;
+import static org.lwjgl.glfw.GLFW.GLFW_PRESS;
+
 @Environment(EnvType.CLIENT)
 public class ScreenBreakout extends Breakout {
-    private final Screen screen;
+    private final @NotNull Screen screen;
     private boolean isClosing;
     private double x;
     private double y;
     private final MinecraftClient client;
     private final Queue<Runnable> queue = new ConcurrentLinkedQueue<>();
+    private int controlLeftTicks;
+    private int activeButton;
+    private int field_1796;
+    private double glfwTime;
 
-    public ScreenBreakout(Identifier breakoutId, Screen screen) {
+    public ScreenBreakout(Identifier breakoutId, @NotNull Screen screen) {
         super(breakoutId, new BreakoutWindow(screen.getTitle().getString(), screen.width, screen.height));
         this.screen = screen;
         this.client = Screens.getClient(screen);
         // TODO: make these run in render or update instead of on the main thread
         InputUtil.setMouseCallbacks(window.getHandle(),
                 (window, x, y) -> this.client.execute(() -> this.onCursorPos(window, x, y)),
-                (window, button, action, mods) -> this.client.execute(() -> this.onMouseButton(window, button, action, mods)),
+                (window, button, action, mods) -> this.execute(() -> this.onMouseButton(window, button, action, mods)),
                 (window, xOffset, yOffset) -> this.client.execute(() -> this.onMouseScroll(window, xOffset, yOffset)),
                 (window, count, names) -> {
                     Path[] paths = new Path[count];
@@ -60,6 +71,48 @@ public class ScreenBreakout extends Breakout {
     }
 
     private void onMouseButton(long window, int button, int action, int mods) {
+        if (window == this.client.getWindow().getHandle()) {
+            boolean bl = action == GLFW_PRESS;
+            if (MinecraftClient.IS_SYSTEM_MAC && button == 0) {
+                if (bl) {
+                    if ((mods & GLFW_MOD_CONTROL) == GLFW_MOD_CONTROL) {
+                        button = 1;
+                        ++this.controlLeftTicks;
+                    }
+                } else if (this.controlLeftTicks > 0) {
+                    button = 1;
+                    --this.controlLeftTicks;
+                }
+            }
+
+            if (bl) {
+                if (this.client.options.touchscreen && this.field_1796++ > 0) {
+                    return;
+                }
+
+                this.activeButton = button;
+                this.glfwTime = GlfwUtil.getTime();
+            } else if (this.activeButton != -1) {
+                if (this.client.options.touchscreen && --this.field_1796 > 0) {
+                    return;
+                }
+
+                this.activeButton = -1;
+            }
+
+            if (this.client.overlay == null) {
+                double d = this.x * (double)this.client.getWindow().getScaledWidth() / (double)this.client.getWindow().getWidth();
+                double e = this.y * (double)this.client.getWindow().getScaledHeight() / (double)this.client.getWindow().getHeight();
+                final int finalButton = button;
+                ScreenContextTracker.pushContext(ScreenContextTracker.ScreenContextElement.ScreenEventType.MOUSE_BUTTON, ((ScreenAccessor)screen).multi_window_getTreeElement());
+                if (bl) {
+                    Screen.wrapScreenError(() -> this.screen.mouseClicked(d, e, finalButton), "mouseClicked event handler", this.screen.getClass().getCanonicalName());
+                } else {
+                    Screen.wrapScreenError(() -> this.screen.mouseReleased(d, e, finalButton), "mouseReleased event handler", this.screen.getClass().getCanonicalName());
+                }
+                ScreenContextTracker.popContext();
+            }
+        }
     }
 
     private void onMouseScroll(long window, double xOffset, double yOffset) {
--- src/main/java/de/kb1000/multiwindow/mixin/client/MinecraftClientMixin.java
+++ src/main/java/de/kb1000/multiwindow/mixin/client/MinecraftClientMixin.java
@@ -11,7 +11,6 @@ import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.TitleScreen;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.NotNull;
-import org.jetbrains.annotations.Nullable;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
